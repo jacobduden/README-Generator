99a1af2fe2931a3d7c8c8692f1a5cca992194127
jacobduden <75395688+jacobduden@users.noreply.github.com>
2021-04-26 20:16:57 -0500

    created questions

---
+++ index.js
@@ -0,0 +1,69 @@
+// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const axios = require("axios");
+const generate = require("./utilities/generateMarkdown");
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is your project title?"
+    },
+    {
+        type: "input",
+        name: "badge",
+        message: "please insert badge links that you want."
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please provide your project's description.]"
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Please provide installation instructions to use your project."
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Provide the project usuage."
+    },
+    {
+        type: "input",
+        name: "license",
+        message: "Please provide the project licence."
+    },
+    {
+        type: "input",
+        name: "contributor",
+        message: "Provide contributing parties to the project."
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "please provide the tests for the project."
+    },
+    {
+        type: "input",
+        name: "username",
+        message: "What is your Github username?"
+    },
+    {
+        type: "input",
+        name: "repo",
+        message: "Please provide your Github repository link."
+    }
+];
+ 
+
+
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
+
+// // TODO: Create a function to initialize app
+// function init() {}
+
+// // Function call to initialize app
+// init();

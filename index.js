58ce3c46c7c01f3177f378451dee7241f56621ca
jacobduden <75395688+jacobduden@users.noreply.github.com>
2021-04-26 20:17:39 -0500

    created function to write the readme file

--- index.js
+++ index.js
@@ -57,7 +57,27 @@ const questions = [
     }
 ];
  
+inquirer
+.prompt(questions)
+.then(function(data){
+    const queryUrl = `https://api.github.com/users/${data.username}`;
 
+    axios.get(queryUrl).then(function(res){
+
+        const githubInformation = {
+            githubImage: res.data.avatar_url,
+            email: res.data.email,
+            profile: res.data.html_url,
+            name: res.data.name
+        };
+        fs.writeFile("README.md", generate(data, githubInformation), function(err){
+            if (err) {
+                throw err;
+            };
+            console.log("New README.md file was created successfully!");
+        })
+    })
+})
 
 // // TODO: Create a function to write README file
 // function writeToFile(fileName, data) {}
